f099da3355863d1f73bcae9964ad1c7d82e17fa1
Toshio Iwai <iwai@gol.com>
2025-12-04 15:11:15 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 let uX, uY; // タッチしているポイントの座標
-let cX, cY; // 回転のセンター座標
+let cX, cY; // 回転検出のセンター座標
+let pX, pY; // 表示のセンター座標
+let circle_in, circle_out; // タッチする円の内径、外径
+let rect_length,rect_width; // 表示する四角の長さと幅
 
 function setup() {
 
@@ -17,19 +20,7 @@ function setup() {
 
 function draw() {
   background(200);
-
-  if (deviceOrientation === 'portrait') {
-    // 縦向きの場合の処理
-    textSize(20);
-    textAlign(CENTER, CENTER);
-    text('現在は縦向きです', width / 2, height / 2);
-  } else if (deviceOrientation === 'landscape') {
-    // 横向きの場合の処理
-    textSize(20);
-    textAlign(CENTER, CENTER);
-    text('現在は横向きです', width / 2, height / 2);
-  }
-
+  
   // タッチがある場合はタッチ座標を使用し、ない場合はマウス座標を使用
 
      if (touches.length > 0) {
@@ -40,18 +31,38 @@ function draw() {
         uY = mouseY;
       }
 
-  cX = ww/2;
-  cY = wh/4*3;
-  
+  if(ww < wh)
+  {
+    cX = ww/2;
+    cY = wh/6*5;
+    pX = ww/2;
+    pY = wh/6*2;
+    circle_in = ww/3;
+    circle_out = ww/5;
+    rect_length = ww*0.8;
+    rect_width = ww*0.1;
+  }
+  else
+  {
+    cX = ww/6*5;
+    cY = wh/2;
+    pX = ww/6*2;
+    pY = wh/2;
+    circle_in = wh/3;
+    circle_out = wh/5;
+    rect_length = wh*0.8;
+    rect_width = wh*0.1;
+  }
+    
   push();
   translate(cX, cY);
   noStroke();
   fill(255,200,200);
-  ellipse(0,0,ww/3,ww/3);
+  ellipse(0,0,circle_in,circle_in);
   
   noStroke();
   fill(200);
-  ellipse(0,0,ww/5,ww/5);
+  ellipse(0,0,circle_out,circle_out);
   pop();
   
   // 原点に対するマウスの座標を取得します
@@ -63,14 +74,14 @@ function draw() {
 
   push();
   // 原点を中心に移動します
-  translate(ww/2, wh/4*1);
+  translate(pX, pY);
 
   // 回転させます
   rotate(a);
 
   // 図形を描画します
   fill(255);
-  rect(-ww*0.4, -wh*0.05, ww*0.8, wh*0.1);
+  rect(-rect_length/2, -rect_width/2, rect_length, rect_width);
   pop();
   
 }
